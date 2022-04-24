c47132c37c7b3bac4617496a639adf604357a5a4
CertifiedJoon <u3566665@connect.hku.hk>
2022-04-24 23:44:26 +0000

    added components

--- modern_react/github-finder/src/App.js
+++ modern_react/github-finder/src/App.js
@@ -1,8 +1,26 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
+import NavBar from './components/layouts/NavBar'
+import Footer from './components/layouts/Footer'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-sl">Hello World</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <NavBar/>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
